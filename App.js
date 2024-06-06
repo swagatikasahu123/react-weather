4fb29f7cb0b8f43df194e5111d0cdc46bb86dafb
swagatikasahu123 <155278112+swagatikasahu123@users.noreply.github.com>
2024-06-06 13:11:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+//import React from "react";
+// import Resturant from "./component/Basics/Resturant";cd 
+ //import UseState from "./component/Hooks/useState";
+// import UseEffect from "./component/Hooks/useEffect";
+//import UseReducer from "./component/Hooks/useReducer";
+
+/*const App = () => {
+  return <UseState />;
+};
+
+export default App;*/
+import React from 'react';
+//import UseState from "./component/Hooks/useState";
+//import UseEffect from './component/Hooks/useEffect';
+//import UseReducer from './component/Hooks/useReducer';
+//import Todo from './component/todoreact/todo';
+import Temp from './component/weather/temp';
+const App = () => {
+  return (
+    <>
+     <Temp/> 
+  </>
+  )
+}
+
+export default App;
