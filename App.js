b66ef9bb5588726966fc09d0f75eed7647bf90d6
swagatikasahu123 <155278112+swagatikasahu123@users.noreply.github.com>
2024-06-06 13:12:15 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,6 @@
-//import React from "react";
-// import Resturant from "./component/Basics/Resturant";cd 
- //import UseState from "./component/Hooks/useState";
-// import UseEffect from "./component/Hooks/useEffect";
-//import UseReducer from "./component/Hooks/useReducer";
 
-/*const App = () => {
-  return <UseState />;
-};
-
-export default App;*/
 import React from 'react';
-//import UseState from "./component/Hooks/useState";
-//import UseEffect from './component/Hooks/useEffect';
-//import UseReducer from './component/Hooks/useReducer';
-//import Todo from './component/todoreact/todo';
+
 import Temp from './component/weather/temp';
 const App = () => {
   return (
